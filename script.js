d9249605eb94adfb5a5a8274b2d56da51f852e85
Lyhuwu <jeydon45@gmail.com>
2025-09-24 23:41:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener("click", () => {
-  const llamas = document.querySelectorAll(".flame");
+  const llamas = document.querySelectorAll(".flame-wrap");
   llamas.forEach(llama => {
-    llama.classList.add("off"); // Apaga las llamas
+    llama.style.animation = "none";           // Detiene el parpadeo
+    llama.style.backgroundColor = "#7a3c00"; // Color más apagado
+    llama.style.opacity = "0.6";              // Llama más tenue
   });
 });
