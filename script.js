ef300760c1a5212fe833dbd625c0b62dc4386935
Lyhuwu <jeydon45@gmail.com>
2025-09-25 20:03:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-document.addEventListener("click", () => {
-  const llamas = document.querySelectorAll(".flame-wrap");
-  llamas.forEach(llama => {
-    llama.classList.toggle("off"); // Apaga/enciende la vela sin desaparecer
-  });
-});
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.4.2/gsap.min.js"></script>
